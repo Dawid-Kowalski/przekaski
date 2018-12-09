60463d18a560d7e97fe97556c0d2426386cd1c90
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-09 09:15:32 +0100

    Add second template and show/hide function for recept

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 'use strict';
 (function(){ 
 
-	let template = document.getElementById('template').innerHTML;
+	let mealTemplate = document.getElementById('meal-template').innerHTML;
+	let reciptTemplate = document.getElementById('recipt-template').innerHTML;
 	let carousel = document.getElementById('carousel');
+	let recipt = document.getElementById('recipt');
 
-	Mustache.parse(template);
+	Mustache.parse(mealTemplate);
+	Mustache.parse(reciptTemplate);
 
 	let mealData = 
 					[
@@ -21,7 +24,7 @@
 							image: 'images/pieczarkimin.jpg'
 						},
 						{	
-							title: 'mufinki - pizzerinki',
+							title: 'pizzerinki',
 							image: 'images/pizzerinkimin.jpg'
 						},
 						{	
@@ -29,10 +32,62 @@
 							image: 'images/zawijancemin.jpg'
 						}						
 					];
+
+	let reciptData = 
+					[
+						{	
+							id: 0,
+							title: 'kurczaki w panierce'
+						},
+						{	
+							id: 1,
+							title: 'quesadilla'
+						},
+						{	
+							id: 2,
+							title: 'zapiekane pieczarki'
+						},
+						{	
+							id: 3,
+							title: 'pizzerinki'
+						},
+						{	
+							id: 4,
+							title: 'zawijańce z tortilli'
+						}	
+					];
 		
 	for(let i=0; i<mealData.length; i++) {
-		let generatedMeal = Mustache.render(template, mealData[i]);
+		let generatedMeal = Mustache.render(mealTemplate, mealData[i]);
 		carousel.insertAdjacentHTML('beforeend', generatedMeal);
 	}
-	
+
+	for(let i=0; i<reciptData.length; i++) {
+		let generatedRecipt = Mustache.render(reciptTemplate, reciptData[i]);
+		recipt.insertAdjacentHTML('beforeend', generatedRecipt);
+	}
+
+
+
 })();
+
+const carousel = document.querySelector('.carousel');
+
+const flkty = new Flickity(carousel, {
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false
+});
+
+let recipes = [];
+for(let i=0; i<5; i++){
+	let recipe = document.getElementById(i);
+	recipes.push(recipe);
+}
+
+flkty.on('change', function(index){
+	for(let i=0; i<recipes.length; i++){
+			recipes[i].hidden = true;
+		}
+	recipes[index].hidden = false;
+});
